98db4a1d0be1fd21e870107889eabfb6cf4d7854
Danylo Kiral <kiral.danylo@gmail.com>
2021-05-30 01:27:32 +0300

    Implemented visualization script, improved readme

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
-# kafka-throughput-investigation
+# Kafka throughput investigation
 
-##Instructions 
+##Description
+
+Project was implemented to investigate throughput of a kafka-based solution considering different number of producers, consumers, partitions  and replicas
+
+Consists of following components:
+- “Generator” microservice that splits the dataset to messages (reddit comments), sends them to kafka as message. 
+- Microservice that receives messages (reddit comments), imitates processing by sleeping for 1 sec and saves to MongoBD message data (time when started/finished processing and message size).
+- Microservice that aggregates message statistics from MongoBD and generates a report.
+
+Experiments performed:
+1. One producer, a topic with one partition, one consumer
+2. One producer, a topic with one partition, 2 consumers
+3. One producer, a topic with one partition, 5 consumers
+4. One producer, a topic with one partition, 10 consumers
+5. One producer, a topic with 2 partitions, 2 consumers
+6. One producer, a topic with 2 partitions, 5 consumers
+7. One producer, a topic with 2 partitions, 10 consumers
+8. One producer, a topic with 10 partitions, 10 consumers
+9. 2 producers, a topic with 10 partitions, 10 consumers
+
+##How to run 
 1. Have **java, docker, sbt** installed.
 2. Unarchive "RC_2009-04.bz2" file (or any other file with Reddit comments from http://files.pushshift.io/reddit/comments/) to 'data' folder.
 3. Run **start.sh** script.
--- data-visualization.py
+++ data-visualization.py
@@ -1,12 +1,72 @@
 
-
-configsThroughputs = [2.4789922377642463E-4]
-configsLatencies = [299]
-
-all = [
-    {
+data = [   
+    # 1 partition
+# 1 consumer
+{
     "avgThroughputMbps": 2.4789922377642463E-4,
     "maxLatencyMs": 299
 },
-    
+# 2 consumer
+{
+    "avgThroughputMbps": 2.504233324562095E-4,
+    "maxLatencyMs": 307
+},
+# 5 consumer
+{
+    "avgThroughputMbps": 2.574074888083548E-4,
+    "maxLatencyMs": 297
+},
+# 10 consumer
+{
+    "avgThroughputMbps": 2.486620630536761E-4,
+    "maxLatencyMs": 305
+},
+
+    # 2 partitions
+# 2 consumer
+{
+    "avgThroughputMbps": 4.622836312847937E-4,
+    "maxLatencyMs": 291
+},
+# 5 consumer
+{
+    "avgThroughputMbps": 4.701061946589772E-4,
+    "maxLatencyMs": 295
+},
+# 10 consumer
+{
+    "avgThroughputMbps": 4.711999483635089E-4,
+    "maxLatencyMs": 289
+},
+
+# 10 partitions, 10 consumers
+{
+    "avgThroughputMbps": 0.0019595891060913808,
+    "maxLatencyMs": 245
+},
+
+# 2 producers, 10 partitions, 10 consumers
+{
+    "avgThroughputMbps": 0.0021131759466126907,
+    "maxLatencyMs": 275
+}
 ]
+
+throughputs = list(map(lambda x: x["avgThroughputMbps"], data))
+maxLatency = list(map(lambda x: x["maxLatencyMs"], data))
+
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(10, 7))
+plt.plot(list(range(1, len(data) + 1)), throughputs)
+plt.xlabel('# of experiment')
+plt.ylabel('Throughput (Mbps)')
+plt.title('Throughput vs Configuration')
+plt.show()
+
+plt.figure(figsize=(10, 7))
+plt.plot(list(range(1, len(data) + 1)), maxLatency)
+plt.xlabel('# of experiment')
+plt.ylabel('Max latency (ms)')
+plt.title('Max latency vs Configuration')
+plt.show()
